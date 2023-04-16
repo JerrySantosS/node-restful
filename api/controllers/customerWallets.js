617b7c3c68d091345730b95e0b265d40e0f247a8
JerrySantosS <jerryrock98@gmail.com>
2023-04-16 14:22:18 -0300

    Adição dos métodos DELETE e PUT

--- api/controllers/customerWallets.js
+++ api/controllers/customerWallets.js
@@ -1,4 +1,4 @@
-const uuidv4 = require('uuid/v4');
+const {v4: uuidv4} = require('uuid');
 
 module.exports = app => {
   // const customerWalletsDB = require('../data/customerWallets.json');
@@ -16,7 +16,7 @@ module.exports = app => {
   controller.saveCustomerWallets = (req, res) => {
     customerWalletsMock.data.push({
       id: uuidv4(),
-      parentID: uuidv4(),
+      parentId: uuidv4(),
       name: req.body.name,
       birthDate: req.body.birthDate,
       cellPhone: req.body.cellPhone,
@@ -28,6 +28,66 @@ module.exports = app => {
 
     res.status(201).json(customerWalletsMock)
   }
+
+  controller.removeCustomerWallets = (req, res) => {
+    const { customerId, } = req.params;
+
+    const foundCustomerIndex = customerWalletsMock.data.findIndex(
+      customer => customer.id === customerId);
+
+    if (foundCustomerIndex === -1) {
+      res.status(404).json({
+        message: 'cliente não encontrado na base.',
+        succcess: false,
+        customerWallets: customerWalletsMock,
+      })
+    }else {
+      customerWalletsMock.data.splice(foundCustomerIndex, 1);
+      res.status(200).json({
+        message: 'cliente encotrado e deletado com sucesso.',
+        success: true,
+        customerWallets: customerWalletsMock,
+      });
+    }
+
+  }
+
+  controller.updateCustomerWallets = (req, res) => {
+    const { customerId, } = req.params;
+
+    const foundCustomerIndex = customerWalletsMock.data.findIndex(
+      customer => customer.id === customerId);
+
+    if (foundCustomerIndex === -1) {
+      res.status(404).json({
+        message: 'cliente não encontrado na base.',
+        succcess: false,
+        customerWallets: customerWalletsMock,
+      })
+    }else {
+      const newCustomer = {
+        id: customerId,
+        parentId: req.body.parentId,
+        name: req.body.name,
+        birthDate: req.body.birthDate,
+        cellPhone: req.body.cellPhone,
+        phone: req.body.phone,
+        email: req.body.email,
+        occupation: req.body.occupation,
+        state: req.body.state,
+        createdAt: new Date()
+      };
+
+      customerWalletsMock.data.splice(foundCustomerIndex, 1, newCustomer);
+      console.log(newCustomer)
+      res.status(200).json({
+        message: 'cliente encotrado e atualizado com sucesso.',
+        success: true,
+        customerWallets: customerWalletsMock,
+      });
+    }
+  }
+
 
   return controller;
 }
